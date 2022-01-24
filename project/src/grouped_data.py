6f09bf47e7b4bd41aad7e8ae1669fada7bec751c
Ardiegon <oskar000001@interia.pl>
2022-01-24 17:14:53 +0100

    Synthetic data | Model Init | Grouped Data Interface

--- project/src/grouped_data.py
+++ project/src/grouped_data.py
@@ -8,11 +8,17 @@ class GroupedData:
         else:
             self.data = self.load_data(data_path)
 
-    def generate_data(self, n_points = 30, box_size = 10, group_size = 2, point_size = (1,50)):
+    def generate_data(self, n_points = 50, box_size = 20, group_size = 4, point_size = (1,50)):
+        point_id = np.arange(n_points) + 1
         x_cor = np.random.rand(n_points)*box_size
         y_cor = np.random.rand(n_points)*box_size
         weight = (np.random.rand(n_points)*(point_size[1]-point_size[0])+point_size[0])
-        points = np.stack((x_cor, y_cor, weight), axis=1)
+        points = np.stack((point_id, x_cor, y_cor, weight), axis=1)
+        start_point = np.expand_dims(np.array([0, box_size/2, box_size/2, 0]), 0)
+        points = np.concatenate((start_point, points), axis=0)
+        start_point = np.squeeze(start_point, axis=0)
+        print(start_point)
+        start_group = 0
 
         groups_in_side = int(box_size / group_size)
         number_of_groups = groups_in_side**2
@@ -34,11 +40,13 @@ class GroupedData:
             group_neighbours.append(neighbours)
             g_points = []
             for point in points:
-                if point[0] >= group_size*(g_col) and point[0] < group_size*(g_col+1) and point[1] >= group_size*(g_row) and point[1] < group_size*(g_row+1):
+                if group_size*(g_col) <= point[1] < group_size*(g_col + 1) and point[2] >= group_size*(g_row) and point[2] < group_size*(g_row + 1):
+                    if point[0] == 0:
+                        start_group = group_id
                     g_points.append(point)
             groups[group_id] = g_points
 
-        return points, groups, group_neighbours
+        return points, groups, group_neighbours, start_point, start_group
 
     def load_data(self, data_path):
         pass
--- project/src/model.py
+++ project/src/model.py
@@ -1,19 +1,38 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import logging
 import time
 from project.src.thread_with_return import ThreadWithReturn
+from random import uniform
 from project.src.grouped_data import GroupedData
 
 class Model:
-    def __init__(self, _n_ants, _pheromone_weight,  _backpack_size, _distance_weight):
+    def __init__(self, _n_ants, _backpack_size, _pheromone_weight, _distance_weight):
+        self.points, self.groups, self.groups_neighbours, self.start_point, self.start_group = (None, None, None, None, None)
         self.n_ants = _n_ants
         self.pheromone_weight = _pheromone_weight
         self.backpack_size = _backpack_size
         self.distance_weight = _distance_weight
 
-    def load_data(self, data: GroupedData):
-        pass
+    def load_data(self, gdata: GroupedData):
+        self.points, self.groups, self.groups_neighbours, self.start_point, self.start_group = gdata.get_data()
+
+    def show_routes(self, groups, routes = None):
+        def randomRGB():
+            return (uniform(0.05,0.8),uniform(0.05,0.8),uniform(0.05,0.8))
+        for group in groups:
+            clr = randomRGB()
+            for point in group:
+                if point[0] == 0:
+                    plt.scatter(point[1], point[2], marker = "X", color = (0.0,0.0,0.0))
+                    plt.annotate("Start", (point[1], point[2]))
+                else:
+                    plt.scatter(point[1], point[2], color = clr)
+        plt.show()
 
 if __name__ == "__main__":
-    data = GroupedData()
+    model = Model(5, 100, 1, 1)
+    model.load_data(GroupedData())
+    model.show_routes(model.groups)
+
